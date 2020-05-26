b7767b508e5b40571ce375fcba7e67654a5fd158
Brennon Nuckols <brennuck@gmail.com>
2020-05-26 14:11:55 -0600

    Logger

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
+server.use(express.json());
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`Request Method: ${req.method}, Request Url: ${req.originalUrl}, TimeStamp: ${new Date().getDate()}`)
+  
+  next();
+}
 
 module.exports = server;
