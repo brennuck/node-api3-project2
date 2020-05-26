66d0450809d8d42e747a6ec3738b1c567e9dd9af
Brennon Nuckols <brennuck@gmail.com>
2020-05-26 14:41:48 -0600

    this should work

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // code away!
 const server = require('./server.js');
 
+require('dotenv').config();
+
 const port = process.env.PORT || 1234;
 server.listen(port, () => {
     console.log(`\n* Server Running on ${port} *\n`);
